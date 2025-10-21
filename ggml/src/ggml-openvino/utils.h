11336778880612e7a7549c921cd1fb86669e7171
Yu, Zijun <zijun.yu@intel.com>
2025-10-21 10:57:03 +0800

    Minor udpates

--- ggml/src/ggml-openvino/utils.h
+++ ggml/src/ggml-openvino/utils.h
@@ -42,7 +42,7 @@ const ggml_tensor* get_inp_pos_tensor(struct ggml_cgraph* cgraph);
 
 bool get_is_first_token(const ggml_tensor* inp_pos);
 
-std::vector<std::pair<std::string, ggml_tensor*>> get_kv_tensors(struct ggml_cgraph* cgraph);
+std::unordered_map<std::string, ggml_tensor*> get_kv_tensors(struct ggml_cgraph* cgraph);
 
 ov::AnyMap get_npu_prefill_config();
 ov::AnyMap get_npu_generate_config();
