77bd3541d823903667612ffea0631cad1bb6cc33
Mustafa Cavus <mustafa.cavus@intel.com>
2026-04-08 18:37:18 -0700

    Gelu tanh support (#125)

--- ggml/src/ggml-openvino/ggml-openvino.cpp
+++ ggml/src/ggml-openvino/ggml-openvino.cpp
@@ -935,6 +935,7 @@ static bool ggml_backend_openvino_device_supports_op(ggml_backend_dev_t dev, con
                                                  // GGML_OP_SOFT_MAX,
                                                  GGML_OP_SET_ROWS, GGML_OP_FLASH_ATTN_EXT, GGML_OP_CPY};
     static const std::set<ggml_unary_op> supported_unary_ops{
+        GGML_UNARY_OP_GELU,
         GGML_UNARY_OP_SILU,
     };
     static const std::set<ggml_glu_op> supported_glu_ops{
