452709c18e2f57d66a0aa4a10be4b11cf3a06b33
Mustafa Cavus <mustafa.cavus@intel.com>
2026-05-05 19:32:33 -0700

    1to1 match op update (#146)
    * added translate_1to1_match_1_input function and updated gelu and tanh translations
    
    * Remove unused translation function calls
    
    ---------
    
    Co-authored-by: Mustafa Cavus <mustafacavus@intel.com>

--- ggml/src/ggml-openvino/openvino/op_table.h
+++ ggml/src/ggml-openvino/openvino/op_table.h
@@ -10,10 +10,8 @@ namespace op {
 
 #define GGML_OP_CONVERTER(op) OutputVector op(const NodeContext& context)
 
-GGML_OP_CONVERTER(translate_add);
 GGML_OP_CONVERTER(translate_cont);
 GGML_OP_CONVERTER(translate_get_rows);
-GGML_OP_CONVERTER(translate_mul);
 GGML_OP_CONVERTER(translate_mulmat);
 GGML_OP_CONVERTER(translate_permute);
 GGML_OP_CONVERTER(translate_reshape);
@@ -22,8 +20,6 @@ GGML_OP_CONVERTER(translate_norm);
 GGML_OP_CONVERTER(translate_rope);
 GGML_OP_CONVERTER(translate_scale);
 GGML_OP_CONVERTER(translate_unary_silu);
-GGML_OP_CONVERTER(translate_unary_gelu);
-GGML_OP_CONVERTER(translate_unary_tanh);
 GGML_OP_CONVERTER(translate_soft_max);
 GGML_OP_CONVERTER(translate_transpose);
 GGML_OP_CONVERTER(translate_view);
