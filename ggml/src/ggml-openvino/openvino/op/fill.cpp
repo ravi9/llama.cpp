75ff066dc14897254f2e4c3ab974066b80af9273
Yu, Zijun <zijun.yu@intel.com>
2026-05-21 16:33:46 +0800

    add concat ssm_conv in compute_dynamic_dim
    enable qwen35
    
    Fix after rebase
    
    remove logging

--- ggml/src/ggml-openvino/openvino/op/fill.cpp
+++ ggml/src/ggml-openvino/openvino/op/fill.cpp
@@ -2,42 +2,29 @@
 #include "../op_table.h"
 #include "../utils.h"
 
-#include <cstring>
-#include <cstdint>
-#include <memory>
 #include <openvino/op/broadcast.hpp>
 #include <openvino/op/constant.hpp>
-#include <openvino/op/shape_of.hpp>
-#include <vector>
 
 namespace ov {
 namespace frontend {
 namespace ggml {
 namespace op {
 
+// GGML FILL sets all elements of a tensor to a constant value.
+// The constant is stored as a float in op_params[0].
 OutputVector translate_fill(const NodeContext & context) {
     num_inputs_check(context, 1, 1);
 
-    const int32_t * op_params = context.get_output_op_params();
-    FRONT_END_CHECK_IMPLEMENTED(op_params != nullptr, "FILL requires output op params");
+    float c;
+    memcpy(&c, context.get_output_op_params(), sizeof(float));
 
-    float value;
-    std::memcpy(&value, op_params, sizeof(float));
+    auto shape = context.get_input_shape(0).to_shape();
 
-    auto scalar = ov::op::v0::Constant::create(context.get_output_type(), ov::Shape{}, {value});
+    auto val = ov::op::v0::Constant::create(ov::element::f32, {}, {c});
+    auto target_shape = ov::op::v0::Constant::create(ov::element::i64, {shape.size()},
+        std::vector<int64_t>(shape.begin(), shape.end()));
+    auto res = std::make_shared<ov::op::v3::Broadcast>(val, target_shape);
 
-    ov::Output<ov::Node> target_shape;
-    const auto output_shape = context.get_output_shape();
-    if (output_shape.rank().is_static() && output_shape.is_static()) {
-        const auto static_shape = output_shape.to_shape();
-        std::vector<int64_t> shape_values(static_shape.begin(), static_shape.end());
-        target_shape = ov::op::v0::Constant::create(ov::element::i64, {shape_values.size()}, shape_values);
-    } else {
-        auto input = process_view_input_new(context, 0);
-        target_shape = std::make_shared<ov::op::v3::ShapeOf>(input, ov::element::i64);
-    }
-
-    auto res = std::make_shared<ov::op::v3::Broadcast>(scalar, target_shape);
     return rename_outputs_with_suffix({res}, context.get_name());
 }
 
