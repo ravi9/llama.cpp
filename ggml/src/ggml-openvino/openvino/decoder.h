ba7db56b7ea285d072d591c507274019e8bb57b7
Mostafa Faheem <mostafaaafaheem@gmail.com>
2026-06-19 03:51:46 +0300

    OpenVINO Backend: Remove compute_op_type hardcoded sets

--- ggml/src/ggml-openvino/openvino/decoder.h
+++ ggml/src/ggml-openvino/openvino/decoder.h
@@ -75,8 +75,6 @@ public:
 
     virtual std::vector<std::string> get_output_names(int node_idx) const = 0;
 
-    virtual const std::string & get_op_type() const = 0;
-
     virtual const std::string & get_op_type(int node_idx) const = 0;
 
     virtual const std::string & get_op_name() const = 0;
